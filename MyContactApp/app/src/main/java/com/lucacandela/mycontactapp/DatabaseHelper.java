34e291f1a07926f586c4c4520f8dd1314d3867ba
Luca Candela <lucacandela1959@gmail.com>
2019-05-28 09:12:37 -0700

    Instantiated the project setup suff

---
+++ MyContactApp/app/src/main/java/com/lucacandela/mycontactapp/DatabaseHelper.java
@@ -0,0 +1,41 @@
+package com.lucacandela.mycontactapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "LC_ContactDB.db";
+    public static final String TABLE_NAME = "LC_ContactDB_table";
+    public static final String ID = "ID";
+    public static final String COLUMN_NAME_CONTACT = "contact";
+
+    public static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    COLUMN_NAME_CONTACT + " TEXT)";
+
+    public static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        Log.d("MyContactApp","DatabaseHelper: constructed DatabaseHelper");
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL(SQL_CREATE_ENTRIES);
+        Log.d("MyContactApp","DatabaseHelper: created database");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_ENTRIES);
+        Log.d("MYContactApp","DatabaseHelper: upgraded database");
+    }
+}
--- MyContactApp/app/src/main/java/com/lucacandela/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/lucacandela/mycontactapp/MainActivity.java
@@ -2,12 +2,17 @@ package com.lucacandela.mycontactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    DatabaseHelper myDb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MyContactApp","MainActivity: Setting up the layout");
         setContentView(R.layout.activity_main);
+
+        myDb = new DatabaseHelper(this);
+        Log.d("MyContactApp","MainActivity: Instantiated DatabaseHelper");
     }
 }
