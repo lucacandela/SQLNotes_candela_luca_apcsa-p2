1e4b8983ce795080dfdbdbe2e6ae98766911e4f3
Luca Candela <lucacandela1959@gmail.com>
2019-05-30 10:10:53 -0700

    finished search method

--- MyContactApp/app/src/main/java/com/lucacandela/mycontactapp/DatabaseHelper.java
+++ MyContactApp/app/src/main/java/com/lucacandela/mycontactapp/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.lucacandela.mycontactapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -13,12 +14,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String TABLE_NAME = "LC_ContactDB_table";
     public static final String ID = "ID";
     public static final String COLUMN_NAME_CONTACT = "contact";
-    public static final String COLUMN_NAME_PHONE_NUM = "phoneNumber";
+    public static final String COLUMN_NAME_PHONE_NUM = "phone";
+    public static final String COLUMN_NAME_ADDRESS = "address";
 
     public static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE " + TABLE_NAME + " (" +
                     ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-                    COLUMN_NAME_CONTACT + " TEXT," + COLUMN_NAME_PHONE_NUM +" TEXT)" ;
+                    COLUMN_NAME_CONTACT + " TEXT," + COLUMN_NAME_PHONE_NUM +" TEXT," + COLUMN_NAME_ADDRESS + " TEXT)" ;
 
     public static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + TABLE_NAME;
@@ -44,11 +46,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         Log.d("MyContactApp","DatabaseHelper: 2/2 upgraded database!");
     }
 
-    public boolean insertData(String name,String number){
+    public boolean insertData(String name,String number,String address){
         Log.d("MyContactApp","DatabaseHelper: 1/2 Inserting data...");
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(COLUMN_NAME_CONTACT,name);
+        contentValues.put(COLUMN_NAME_PHONE_NUM, number);
+        contentValues.put(COLUMN_NAME_ADDRESS,address);
         long result = db.insert(TABLE_NAME,null,contentValues);
 
         if (result != -1){
@@ -59,5 +63,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             Log.d("MyContactApp", "DatabaseHelper: Contact insert \"" + name + "\" - FAILED");
             return false;
         }
+    }
+
+    public Cursor getAllData(){
+        Log.d("MyContactApp","DatabaseHelper: 1/2 Getting all data...");
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor result = db.rawQuery("select * from " + TABLE_NAME, null);
+
+
+        return result;
     }
 }
--- MyContactApp/app/src/main/java/com/lucacandela/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/lucacandela/mycontactapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.lucacandela.mycontactapp;
 
+import android.app.AlertDialog;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.lucacandela.mycontactapp.DatabaseHelper.COLUMN_NAME_CONTACT;
 import static com.lucacandela.mycontactapp.DatabaseHelper.TABLE_NAME;
@@ -37,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         String name = editName.getText().toString();
         String phone = editNum.getText().toString();
         String address = editAddress.getText().toString();
-        boolean isInserted = myDb.insertData(name,phone);
+        boolean isInserted = myDb.insertData(name,phone,address);
 
         if (isInserted){
             Toast.makeText(MainActivity.this, "Success - contact inserted",Toast.LENGTH_LONG).show();
@@ -46,4 +51,183 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this, "Failure - contact not inserted",Toast.LENGTH_LONG).show();
         }
     }
+
+    public void viewData(View v){
+        Cursor res = myDb.getAllData();
+        //Log.d("MyContactApp","MainActivity: ");
+
+
+        if (getData() !=null) {
+            String[] results = getData().toString().split("--");
+
+            String result = "";
+            for (String s : results){
+
+                result+= formatContactEntry(s);
+            }
+            showMessage("Contact List", result);
+        }
+        else
+            showMessage("Error","No data found in db");
+    }
+
+    public StringBuffer getData(){
+        Cursor res = myDb.getAllData();
+        if(res.getCount() == 0){
+            return null;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        while(res.moveToNext()){
+            //append res coulmns to the buffer -- see stringbuffer and cursor API's
+            buffer.append(
+                    res.getString(0) + ";" +
+                            res.getString(1) + ";" +
+                            res.getString(2) + ";" +
+                           res.getString(3) + ";--");
+        }
+        return buffer;
+
+    }
+
+    public String formatContactEntry(String s){
+        String out ="";
+
+        String[] entry = s.split(";");
+        String[] mod = new String[entry.length];
+        mod[0] = "ID: " + entry[0];
+        mod[1] = "Contact: " + entry[1];
+        mod[2] = "Phone number: " + entry[2];
+        mod[3] = "Address: " + entry[3];
+
+        for (String x : mod){
+            out+= x + "\n";
+        }
+        out+="\n";
+        return out;
+    }
+
+    public void showMessage(String title, String msg){
+        Log.d("MyContactApp","ActivityMonitor: Building dialogue window");
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title);
+        builder.setCancelable(true);
+        builder.setMessage(msg);
+        builder.show();
+    }
+
+    public void search(View v){
+        Log.d("MyContactApp","ActivityMonitor: Searching for parameters");
+        String name = editName.getText().toString();
+        String phone = editNum.getText().toString();
+        String address = editAddress.getText().toString();
+
+        if (name.equals(""))
+            name = null;
+        if (phone.equals(""))
+            phone = null;
+        if (address.equals(""))
+            address = null;
+
+        if (!(name == null && phone == null && address == null))
+        searchContacts(name,phone,address);
+        else
+            showMessage("Search Params\nNone","No results");
+    }
+
+    public void searchContacts(String name, String phone, String address){
+        StringBuffer rawData = getData();
+        Log.d("MyContactApp","searchContacts: " + rawData.toString());
+        String[] contacts = rawData.toString().split("--");
+
+        String results = "";
+        int count = 0;
+
+        if(name!=null)
+            name = name.toLowerCase();
+        if(address!= null)
+            address = address.toLowerCase();
+        List<String> returnedStrings = new ArrayList<String>();
+        for (String c : contacts){
+            String[] col = c.split(";");
+            count = 0;
+            for (String  s : col){
+
+                col[count]=s.toLowerCase();
+                count++;
+                Log.d("MyContactApp",s);
+            }
+            if (name != null && phone != null && address != null){
+                Log.d("MyContactApp","all params");
+                if (col[1].contains(name) && col[2].contains(phone) && col[3].contains(address)){
+                    returnedStrings.add(c);
+                }
+
+            }
+            else if (name != null && phone != null){
+                Log.d("MyContactApp","name & phone params");
+                if (col[1].indexOf(name) >= 0 && col[2].indexOf(phone) >=0){
+                    returnedStrings.add(c);
+                }
+            }
+            else if (phone != null && address != null){
+                Log.d("MyContactApp","phone and address params");
+                if (col[2].indexOf(phone) >=0 && col[3].indexOf(address) >=0){
+                    returnedStrings.add(c);
+                }
+            }
+            else if (name != null && address!= null){
+                Log.d("MyContactApp","name and address params");
+                if (col[1].indexOf(name) >= 0 && col[3].indexOf(address) >=0){
+                    returnedStrings.add(c);
+                }
+            }
+            else if (name != null){
+                Log.d("MyContactApp","name param only");
+                if (col[1].contains(name)){
+                    returnedStrings.add(c);
+                }
+                else
+                {
+                    Log.d("MyContactApp","Name param [ " + name + " ] did not match [ " + col[1] + " ] in contacts" );
+                }
+            }
+            else if (phone != null){
+                Log.d("MyContactApp","phone param only");
+                if (col[2].indexOf(phone) >=0){
+                    returnedStrings.add(c);
+                }
+            }
+            else if (address != null){
+                Log.d("MyContactApp","address param only");
+                if (col[3].indexOf(address) >=0){
+                    returnedStrings.add(c);
+                }
+            }
+        }
+
+
+        Log.d("MyContactApp",returnedStrings.toString());
+
+        if (returnedStrings.size() == 0){
+            results+= "No results found... check your search fields again.";
+        }
+        else
+        {
+            for (String r : returnedStrings){
+                results+= formatContactEntry(r);
+            }
+        }
+        String title = "Search Params:\n";
+        if(name!= null)     title+="Name: " + name;
+        if (phone!= null)   title+=", Phone: " + phone;
+        if (address!=null)  title+= ", Address: " + address;
+        if (title.equals("Search Params\n")) title += "none";
+
+
+
+        showMessage(title, results);
+
+    }
+
 }
